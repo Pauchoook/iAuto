97712152dd90ec32edb68f9e4d9e5e3a2c4174f2
Pauchoook <nkolatarasov@mail.ru>
2024-08-04 10:42:21 +0300

    full page

--- src/js/app.js
+++ src/js/app.js
@@ -4,8 +4,12 @@ import "../scss/style.scss";
 import spoller from "./files/spoller.js";
 import burger from "./files/burger.js";
 import headerScroll from "./files/header-scroll.js";
+import map from "./files/map.js";
+import smoothScroll from "./files/smooth-scroll.js";
 
 functions.isWebp();
 spoller();
 burger();
 headerScroll();
+map();
+smoothScroll();
---
+++ src/js/files/smooth-scroll.js
@@ -0,0 +1,26 @@
+import SmoothScroll from "smooth-scroll";
+
+export default function smoothScroll() {
+  document.querySelectorAll(".anchor").forEach((link) => {
+    link.addEventListener("click", function (e) {
+      e.preventDefault();
+
+      let href = this.getAttribute("href").substring(1);
+
+      const scrollTarget = document.getElementById(href);
+
+      if (scrollTarget) {
+        const heightHeader = document.querySelector(".header").clientHeight;
+        const offsetPosition = scrollTarget.getBoundingClientRect().top - heightHeader;
+
+        window.scrollBy({
+          top: offsetPosition,
+          behavior: "smooth",
+        });
+
+        document.querySelector("#burger").classList.remove("open");
+        document.body.classList.remove("body-hidden");
+      }
+    });
+  });
+}
